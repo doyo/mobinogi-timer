f0ddbaafab4217107513c367b78f21c2b64f3a27
Yoonsuh Ki <yski@Yoonsuhs-MacBook-Air.local>
2026-03-02 00:09:16 +0900

    Add Electron desktop app setup

---
+++ electron/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const isMac = process.platform === 'darwin';
+
+  const win = new BrowserWindow({
+    width: 780,
+    height: 600,
+    alwaysOnTop: true,
+    autoHideMenuBar: true,
+    titleBarStyle: isMac ? 'hiddenInset' : 'default',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    }
+  });
+
+  // dev: timer.html is at ../timer.html, packaged: same dir as main.js
+  const htmlPath = app.isPackaged
+    ? path.join(__dirname, 'timer.html')
+    : path.join(__dirname, '../timer.html');
+
+  win.loadFile(htmlPath);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
